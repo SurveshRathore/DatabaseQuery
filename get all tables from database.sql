ee4477af4185628e50ee1ad77c522f546d71ee8b
Survesh Rathore <113424413+SurveshRathore@users.noreply.github.com>
2023-01-11 08:44:28 +0530

    query to get all the tables from a database

---
+++ get all tables from database.sql
@@ -0,0 +1,10 @@
+use FundooNotesDB
+
+----get all the tables from the database
+SELECT * FROM sys.tables
+SELECT name FROM sys.tables
+
+select * from UserTable
+select * from Notes
+select * from LabelTable
+select * from CollabTables
